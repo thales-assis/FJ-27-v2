84a6873bcbcf225cc535aaad349a0ef0b587c41e
Thales <thaleslima-19@hotmail.com>
2019-09-09 21:52:23 -0300

    Refactoring Code

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -14,7 +14,9 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,7 @@ import br.com.alura.forum.model.topic.domain.Topic;
 import br.com.alura.forum.repository.CourseRepository;
 import br.com.alura.forum.repository.TopicRepository;
 import br.com.alura.forum.service.DashboardDataProcessingService;
+import br.com.alura.forum.validator.NewTopicCustomValidator;
 import br.com.alura.forum.vo.CategoriesAndTheirStatisticsData;
 
 @RestController
@@ -45,7 +48,13 @@ public class TopicController {
 	
 	@Autowired
 	private CourseRepository courseRepository;
-
+	
+	@InitBinder("newTopicInputDto")
+	public void initBinder(WebDataBinder binder, @AuthenticationPrincipal User loggedUser) {
+		binder.addValidators(new NewTopicCustomValidator(this.topicRepository, loggedUser));
+	}
+	
+	
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public Page<TopicBriefOutputDto> listTopics(TopicSearchInputDto topicSearch,
             @PageableDefault(sort = "creationInstant", direction = Sort.Direction.DESC) Pageable pageRequest) {
--- src/main/java/br/com/alura/forum/validator/NewTopicCustomValidator.java
+++ src/main/java/br/com/alura/forum/validator/NewTopicCustomValidator.java
@@ -1,6 +1,5 @@
 package br.com.alura.forum.validator;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -9,6 +8,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import br.com.alura.forum.controller.dto.input.NewTopicInputDto;
+import br.com.alura.forum.model.PossibleSpam;
 import br.com.alura.forum.model.User;
 import br.com.alura.forum.model.topic.domain.Topic;
 import br.com.alura.forum.repository.TopicRepository;
@@ -32,16 +32,12 @@ public class NewTopicCustomValidator implements Validator {
 	public void validate(Object target, Errors errors) {
 		Instant oneHourAgo = Instant.now().minus(1, ChronoUnit.HOURS);
 		List<Topic> topics = topicRepository.findByOwnerAndCreationInstantAfterOrderByCreationInstantAsc(loggedUser, oneHourAgo);
-		
-		if (topics.size() >= 4) {
-			Instant instantOfTheOldestTopic = topics.get(0).getCreationInstant();
-			long minutesToNextTopic = Duration.between(oneHourAgo, instantOfTheOldestTopic).getSeconds() / 60;
+		PossibleSpam possibleSpam = new PossibleSpam(topics);
+		if (possibleSpam.hasTopicLimitExceeded()) {
+			long minutesToNextTopic = possibleSpam.minutesToNextTopic(oneHourAgo);
 			errors.reject("newTopicInputDto.limit.exceeded", new Object[] { minutesToNextTopic }, "O limite individual de novos tópicos por hora foi excedido");
 		}
+
 	}
-	
+
 }
-	
-	
-	
-	
